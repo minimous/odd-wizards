145f8f16b66c91d09536266a0361ed4d9e67fddf
kazuto <ahmadriswanto89@gmail.com>
2024-12-11 11:14:13 +0700

    fix error config

--- config/config.ts
+++ config/config.ts
@@ -1,26 +1,7 @@
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { env } from 'process';
 
 const getConfig = (network: string) => {
-  switch (network) {
-    case WalletAdapterNetwork.Devnet:
-      return {
-        network: WalletAdapterNetwork.Devnet,
-        endpoint: env.NEXT_PUBLIC_RPC_DEVNET || 'https://api.devnet.solana.com'
-      };
-    case WalletAdapterNetwork.Mainnet:
-      return {
-        network: WalletAdapterNetwork.Mainnet,
-        endpoint: 'https://api.mainnet-beta.solana.com'
-      };
-    case WalletAdapterNetwork.Testnet:
-      return {
-        network: WalletAdapterNetwork.Testnet,
-        endpoint: 'https://api.testnet.solana.com'
-      };
-    default:
-      throw new Error('Invalid network');
-  }
+  return {}
 };
 
 export default getConfig;
