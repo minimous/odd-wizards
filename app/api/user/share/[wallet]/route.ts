b1ee1a4dad65b368c0571b0cdfac9b54efb52865
kazuto <ahmadriswanto89@gmail.com>
2024-12-23 23:59:09 +0700

    update profile page

--- app/api/user/share/[wallet]/route.ts
+++ app/api/user/share/[wallet]/route.ts
@@ -1,36 +1,27 @@
-import getConfig from '@/config/config';
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
-import chromium from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
 
 export async function GET(request: NextRequest, { params }: { params: { wallet: string } }) {
-
-    const config = getConfig();
-
     try {
-        // Gunakan chrome-aws-lambda untuk meluncurkan Chrome
-        const browser = await puppeteer.launch({
-            args: chromium.args,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
+        // Mengirim request untuk mendapatkan gambar
+        const resp = await axios.get(`http://157.173.202.22:3000/screenshot/${params.wallet}`, {
+            responseType: 'arraybuffer', // Mengatur respons untuk menerima buffer data gambar
+            headers: {
+                'Accept': 'image/png', // Mengatur header untuk menerima gambar PNG
+            }
         });
 
-        const page = await browser.newPage();
+        // Memeriksa jika status HTTP adalah 200 (OK)
+        if (resp.status !== 200) {
+            throw new Error('Failed to fetch image from external server');
+        }
 
-        // Go to the provided URL
-        await page.goto(`${config?.base_url}/p/${params.wallet}/share`, { waitUntil: 'networkidle0' });
-
-        // Take a screenshot
-        const screenshot = await page.screenshot({ fullPage: true });
-
-        // Close the browser
-        await browser.close();
-
-        // Return image response
-        return new NextResponse(screenshot, {
+        // Mengembalikan respons gambar dengan header yang sesuai
+        return new NextResponse(resp.data, {
+            status: 200,
             headers: {
                 'Content-Type': 'image/png',
-                'Content-Length': screenshot.length.toString(),
+                'Content-Length': resp.data.length.toString(),
             },
         });
     } catch (error) {
