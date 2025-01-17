16d3c2fbb2c2345953a733fe2ffb272a1da1e9ba
kazuto <ahmadriswanto89@gmail.com>
2025-01-17 15:11:53 +0700

    update v2

---
+++ app/(app)/stake/[collection]/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Header from "@/components/layout/header";
+import CustomGradualSpacing from "@/components/CustomGradouselSpacing";
+import { useState } from "react";
+import { Footer } from "@/components/layout/footer";
+import StakeSection from "@/components/home/StakeSection";
+export default function Stake() {
+
+    return (
+        <div className="relative bg-black w-full">
+            <Header />
+            <div className="bg-[url('/images/stake/banner-odds.png')] bg-cover bg-center w-full h-screen relative">
+                <div className="absolute top-0 w-full h-[250px] bg-gradient-to-b from-black to-transparent" />
+                <div className="absolute bottom-0 w-full h-[250px] bg-gradient-to-b from-transparent to-black" />
+
+                <div className="bg-[url('/images/wizard.gif')] bg-cover bg-center w-[150px] h-[150px] absolute bottom-0 left-1/2 transform -translate-x-1/2 rounded-full" />
+            </div>
+            <div className="flex flex-col items-center justify-center mt-8 px-20">
+                <CustomGradualSpacing
+                    className="font-display text-center text-4xl font-black md:leading-[5rem]"
+                    text="Odds Wizard"
+                />
+                <div className="my-8">
+                    <p className="text-sm md:!text-xl text-gray-400 leading-none">Bad kids is a collection of 9,999 bad drawings of kids. Some people like the pictures and some people are bad kids themselves. You can buy them if you want to or you can just look at them, that’s ok too.</p>
+                </div>
+            </div>
+            <div className="w-full h-[125px] bg-black" />
+            <div>
+                <StakeSection />
+            </div>
+            <div className="bg-[url('/images/bg-line-grid.png')] bg-cover bg-center h-full py-12 md:py-16">
+                <Footer className="my-0" />
+            </div>
+        </div >
+    );
+}
